668bec4041e17d1a15db4174068d34c16b628960
toan301 <toants.301@gmail.com>
2021-04-07 12:48:51 +0700

    update log

--- tests/configs/wdio.shared.conf.js
+++ tests/configs/wdio.shared.conf.js
@@ -49,7 +49,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "info",
+  logLevel: "warn",
   // outputDir: path.resolve(__dirname, 'logs'),
   //
   // Set specific log levels per logger
